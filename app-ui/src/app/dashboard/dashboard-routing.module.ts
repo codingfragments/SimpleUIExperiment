a7df5453295cc8d37027cdbfa314a4d12b49f72a
stefan <smarx@marxworld.net>
2017-09-08 01:08:55 +0200

    LayoutExamples

--- app-ui/src/app/dashboard/dashboard-routing.module.ts
+++ app-ui/src/app/dashboard/dashboard-routing.module.ts
@@ -4,6 +4,19 @@ import { Routes,
 
 import { DashboardComponent } from './dashboard.component';
 import {CustomDashboardComponent} from './custom-dashboard/custom-dashboard.component';
+import {NavConfig, NavEntry, NavSeparator, NavTitle} from "../shared/api/routing-config";
+
+
+export const homeNav = new NavConfig(true)
+  .pushEntry(new NavTitle('Titel1', '', 'icon-speedometer'))
+  .pushEntry(new NavEntry('Title 2', '/test/404', 'icon-user')
+    .addBadge('badge-pill badge-info', '34'))
+  .pushEntry(new NavSeparator())
+  .pushEntry(new NavTitle('Titel1', '', 'icon-speedometer'))
+  .pushEntry(new NavEntry('Master', '/404/77', 'icon-user')
+    .pushEntry(new NavEntry('Test1', '/404/test'))
+    .pushEntry(new NavEntry('Test2', '/404/test')));
+
 
 const routes: Routes = [
   {
@@ -17,7 +30,8 @@ const routes: Routes = [
     path: 'custom',
     component: CustomDashboardComponent,
      data: {
-       t2: 'CustomTest'
+       t2: 'CustomTest',
+       sideNav: homeNav
      }
   },
 
